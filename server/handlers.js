47b0742b5ec449d4945aa85a53f82aa3247543c2
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-06-05 18:44:19 +0400

    feat: move handlers to separate file

---
+++ server/handlers.js
@@ -0,0 +1,78 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const messages = [];
+const maxMessages = 9;
+
+const wss = new WebSocketServer({ noServer: true });
+
+wss.on("connection", (ws) => {
+  ws.send(JSON.stringify({ type: "INIT", messages }));
+});
+
+const handleOptionsRequest = (res) => {
+  res.writeHead(204);
+  res.end();
+};
+
+const handlePostRequest = (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const { message } = JSON.parse(body);
+    if (!message) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Message content is required" }));
+      return;
+    }
+
+    if (messages.length >= maxMessages) {
+      const deletedMessage = messages.shift();
+      broadcast({ type: "DELETE", message: deletedMessage });
+    }
+
+    messages.push(message);
+    broadcast({ type: "ADD", message });
+
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: `Message created ${message}` }));
+  });
+};
+
+const handleGetRequest = (res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ messages }));
+};
+
+const handleNotFound = (res) => {
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ error: "Not found" }));
+};
+
+const broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+const handleUpgradeMap = (request, socket, head) => {
+  return {
+    "/ws": () =>
+      wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit("connection", ws, request);
+      }),
+    default: () => socket.destroy(),
+  };
+};
+
+export {
+  handleOptionsRequest,
+  handlePostRequest,
+  handleGetRequest,
+  handleNotFound,
+  handleUpgradeMap,
+};
--- server/index.js
+++ server/index.js
@@ -1,52 +1,14 @@
 import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
 import url from "url";
+import {
+  handleOptionsRequest,
+  handlePostRequest,
+  handleGetRequest,
+  handleNotFound,
+  handleUpgradeMap,
+} from "./handlers.js";
 
 const PORT = 3001;
-const messages = [];
-const maxMessages = 9;
-
-const handleOptionsRequest = (res) => {
-  res.writeHead(204);
-  res.end();
-};
-
-const sendMessage = (req, res) => {
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-
-  req.on("end", () => {
-    const { message } = JSON.parse(body);
-    if (!message) {
-      res.writeHead(400, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ error: "Message content is required" }));
-      return;
-    }
-
-    if (messages.length >= maxMessages) {
-      const deletedMessage = messages.shift();
-      broadcast({ type: "DELETE", message: deletedMessage });
-    }
-
-    messages.push(message);
-    broadcast({ type: "ADD", message });
-
-    res.writeHead(201, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: `Message created ${message}` }));
-  });
-};
-
-const getMessage = (res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ messages }));
-};
-
-const handleNotFound = (res) => {
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ error: "Not found" }));
-};
 
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -56,40 +18,21 @@ const server = http.createServer((req, res) => {
   const requestHandlerMap = {
     OPTIONS: handleOptionsRequest,
     POST: () =>
-      req.url === "/messages" ? sendMessage(req, res) : handleNotFound(res),
+      req.url === "/messages"
+        ? handlePostRequest(req, res)
+        : handleNotFound(res),
     GET: () =>
-      req.url === "/messages" ? getMessage(res) : handleNotFound(res),
+      req.url === "/messages" ? handleGetRequest(res) : handleNotFound(res),
   };
 
   (requestHandlerMap[req.method] || handleNotFound)(res);
 });
 
-const wss = new WebSocketServer({ noServer: true });
-
-wss.on("connection", (ws) => {
-  ws.send(JSON.stringify({ type: "INIT", messages }));
-});
-
-const broadcast = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(data));
-    }
-  });
-};
-
 server.on("upgrade", (request, socket, head) => {
   const pathname = url.parse(request.url).pathname;
+  const map = handleUpgradeMap(request, socket, head);
 
-  const handleUpgradeMap = {
-    "/ws": () =>
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit("connection", ws, request);
-      }),
-    default: () => socket.destroy(),
-  };
-
-  (handleUpgradeMap[pathname] || handleUpgradeMap["default"])();
+  (map[pathname] || handleUpgradeMap["default"])();
 });
 
 server.listen(PORT, () => {
